0c753713c900f445a6a229ce774058d9c5cab961
drradicevic <drradicevic@gmail.com>
2021-08-31 19:39:31 +0200

    Framework (style bootstrap) added

--- src/entities/Header/Header.jsx
+++ src/entities/Header/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => (
       </ul> */}
 
 <ul>
-        <li><a href="/">Home</></li>
+        <li><a href="/">Home</a></li>
         <li><a href="/authors">Authors</a></li>
         <li><a href="/about">About</a></li>
       </ul>
