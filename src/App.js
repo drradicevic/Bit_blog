ead5b6cd52a9a6f7d93b7fc1cea2fce68817812c
Darko Kovačević <darkokovacevic018@gmail.com>
2021-08-31 19:53:55 +0200

    'in progress'

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Fragment, useEffect } from "react";
 import { Header } from "./entities/Header/Header";
 import { getUsers } from "./services/fetchAPI";
-import{CreatePost} from "./entities/CreatePost/CreatePost.jsx"
-//import { Switch, Route, Redirect } from "react-router-dom";
+import { CreatePost } from "./entities/CreatePost/CreatePost.jsx";
+// import { Switch, Route, Redirect } from "react-router-dom";
 
 import "./App.css";
 
@@ -13,15 +13,15 @@ function App() {
   return (
     <Fragment>
       <Header />
-      <CreatePost/>
-      {/* <Switch>
-        <Route exact path="/" component={HomePage} />
+      <CreatePost />
+      {/* <Switch> */}
+      {/* <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
         <Route exact path="/authors" component={Authors} />
         <Route path="/blog/:id" component={SingleBlogPage} />
         <Route path="/not-found" component={NotFoundPage} />
-        <Redirect from="/" to="/not-found" />
-      </Switch> */}
+        <Redirect from="/" to="/not-found" /> */}
+      {/* </Switch> */}
     </Fragment>
   );
 }
