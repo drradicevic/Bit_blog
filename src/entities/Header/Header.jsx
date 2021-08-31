e2ed99c5f7b15da6b764d0698b97f2fe80288af0
drradicevic <drradicevic@gmail.com>
2021-08-31 19:35:55 +0200

    'Some changes'

--- src/entities/Header/Header.jsx
+++ src/entities/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => (
       {/* <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/authors">Authors</Link></li>
-        <li><Link to="/about">About</Link></li>
+        <li><Link to="/about">About</Link></l>
       </ul> */}
 
 <ul>
