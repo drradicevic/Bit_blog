a352ce0c777a9e71a4e3ad2d7a30d9b6057d0700
marijakov0804 <marija.kovacevic.0804@gmail.com>
2021-09-04 16:23:58 +0200

    created component not found page

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment } from "react";
 import { Header } from "./entities/Header/Header";
-import { getBlogs } from "./services/fetchAPI";
 import { CreatePost } from "./entities/CreatePost/CreatePost.jsx";
 import { HomePage } from "./entities/HomePage/HomePage";
 import { Authors } from "./entities/Authors/Authors";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { BlogPage } from "./entities/BlogPage/BlogPage";
-import { getBlogPage } from "./services/fetchBlogPage";
 import { AuthorPage } from "./entities/AuthorPage/AuthorPage";
 import { About } from "./entities/About/About";
+import { NotFoundPage } from "./entities/NotFoundPage/NotFoundPage";
 
 import "./App.css";
 
 function App() {
-  // useEffect(() => {
-  //   getBlogPage();
-  // });
   return (
     <Fragment>
       <Header />
-      {/*<CreatePost />*/}
       <Switch>
         <Route exact path="/post/new" component={CreatePost} />
         <Route exact path="/" component={HomePage} />
         <Route exact path="/authors" component={Authors} />
         <Route exact path="/author/:id" component={AuthorPage} />
         <Route path="/blog/:id" component={BlogPage} />
-        <Route path="/about" component={About} />
-        {/* 
+        <Route path="/about" component={About} /> 
         <Route path="/not-found" component={NotFoundPage} />
-        <Redirect from="/" to="/not-found" /> */}
+        <Redirect from="/" to="/not-found" />
       </Switch>
     </Fragment>
   );
